9d7c5d5f0c847bde456e146ced60ae69b2882e48
Sergey <example@gmail.com>
2020-11-11 12:19:15 +0100

    Lesson 13: if conditional

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,24 +1,39 @@
 "use strict";
 
-let num = 50;
+if (4 === 4) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
 
-// while (num<=55) {
-//     console.log(num);
-//     num++;
-// }
+const num = 50;
 
-// do {
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Too much');
+} else {
+    console.log('Ok!');
+}
 
-//     console.log(num);
-//     num++;
+//Террнарный оператор
+const num = 50;
+(num == 50) ? console.log('Ok!'): console.log('Error');
+//условие    действие если правда   действие если неправда
+// используется 3 аргумента
 
-// }
-// while (num<55);
-
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
+const num = 40;
+switch (num) {
+    case 49:
+        console.log('Falsch')
+        break;
+    case 39:
+        console.log('Falsch')
+        break;
+    case 50:
+        console.log('Falsch')
+        break;
+    case 40:
+        console.log('Richtig')
+        break;
 }
