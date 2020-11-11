2924fc90283e941cc51896cd56d6d1d9cbc879d9
Sergey <example@gmail.com>
2020-11-11 14:27:54 +0100

    Lesson 16: Functions

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,31 +1,43 @@
 "use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(calc(4, 5))
+// console.log(calc(5, 6))
+// console.log(calc(8, 4))
+// console.log(calc(14, 5))
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+let num = 20;
+
+function showFirstMessage(text) { //Function declarations
+    console.log(text);
+    let num = 10;
+    console.log(num); // Замыкание ф-ци
+}
+showFirstMessage('Hello World!');
+console.log(num);
+
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() { //Function Expression
+    console.log('Hello');
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-if (personalMovieDB.count < 10) {
-    alert("Слишком мало фильмов");
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-    alert("Вы киноман");
-} else if (personalMovieDB.count >= 30) {
-    alert(personalMovieDB);
-} else {
-    alert('Что-то пошло не так');
-}
+logger();
+
+// const calc = (a,b) => a+b;
+// console.log(calc(3,4))
+const calc = (a, b) => {
+    console.log('1');
+    return (a + b);
+};
+console.log(calc(3, 4));
