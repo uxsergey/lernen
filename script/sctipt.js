6716d46539a9bf14c980374a0c198e9ad202c9dd
Sergey <example@gmail.com>
2020-11-12 13:40:46 +0100

    Lesson 20: Objects

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,26 +1,47 @@
 "use strict";
 
-function first() {
-    setTimeout(function() {
-        console.log(1)
-    }, 500);
+const options = { // <--- Object
+    name: 'Test', // <--- Имеет ключ: значение
+    width: 1024,
+    height: 1200,
+    colors: { // <--- Может иметь вложеную структуру
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function() { // <--- Может иметь вложеную функции. Таким образом будем создавать методы обьекта
+        console.log('Test');
+    }
+};
+
+
+
+const { border, background } = options.colors; // Деструктаризация обьекта
+
+console.log(border);
+
+
+console.log(Object.keys(options).length); //Счетчик содержания обьекта. 
+
+//У оьектов бывают встроеные методы и св-Ва
+
+// console.log(options.name)
+
+// delete options.name
+
+// console.log(options);
+
+let counter = 0
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
 }
 
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`я учу ${lang}`)
-    callback();
-}
-
-function done() {
-    console.log('Я прошел этот урок!')
-}
-
-
-learnJS('JavaScript', done)
+console.log(counter)
