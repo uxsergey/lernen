b763585e0a0139069af2bbd11f677aed513e81f1
Sergey <example@gmail.com>
2020-11-16 16:30:03 +0100

    Lesson 26: Dynamic typing

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,80 +1,33 @@
 "use strict";
 
+// To String
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// 1)
 
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-        }
-    },
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Один из последних просмотренных фильмов?', ''),
-                b = prompt('На сколько оцените его?', '');
+console.log(typeof(String(null)));
+console.log(typeof(Number(4)));
 
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-            } else {
-                console.log('error');
-                i--;
-            }
-        }
-    },
-    detectLevel: function() {
+// 2) Concatination
 
-        if (personalMovieDB.count < 10) {
-            alert("Слишком мало фильмов");
-        } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-            alert("Вы киноман");
-        } else if (personalMovieDB.count >= 30) {
-            alert(personalMovieDB);
-        } else {
-            alert('Что-то пошло не так');
-        }
+console.log(typeof(+'5'));
 
-    },
-    showMyDB: function(hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i <= 2; i++) {
-            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            // if (genre === '' || genre == null) {
-            //     console.log('Некорректные данные');
-            //     i--;
-            // } else {
-            //     personalMovieDB.genres[i - 1] = genre;
-            // }
+// 3) 
 
-            let genre = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase;
-            if (genre === '' || genre == null) {
-                console.log('Некорректные данные');
-                i--;
-            } else {
-                personalMovieDB.genres = genre.split(', ');
-                personalMovieDB.genres.sort();
-            }
-        }
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`любимый жанр ${i+1} - это ${item}`);
-        });
+console.log(typeof(parseInt('15px', 10)));
 
-    }
-};
+let answer = +prompt('Hello', "");
+
+// To boolean
+
+// 0, '', null, undefinedm, NaN false
+
+let switcher = 0;
+
+if (switcher) {
+    console.log('Working');
+}
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!"44444"));
