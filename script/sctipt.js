4dd969f9f72013754a6d2bc6ab618b28dbdbf2c1
Sergey <example@gmail.com>
2020-11-16 11:22:11 +0100

    Lesson 21: Array

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,47 +1,36 @@
 "use strict";
 
-const options = { // <--- Object
-    name: 'Test', // <--- Имеет ключ: значение
-    width: 1024,
-    height: 1200,
-    colors: { // <--- Может иметь вложеную структуру
-        border: 'black',
-        background: 'red'
-    },
-    makeTest: function() { // <--- Может иметь вложеную функции. Таким образом будем создавать методы обьекта
-        console.log('Test');
-    }
-};
+const arr = [2, 63, 6, 18];
+arr.sort(compareNum);
+console.log(arr);
 
-
-
-const { border, background } = options.colors; // Деструктаризация обьекта
-
-console.log(border);
-
-
-console.log(Object.keys(options).length); //Счетчик содержания обьекта. 
-
-//У оьектов бывают встроеные методы и св-Ва
-
-// console.log(options.name)
-
-// delete options.name
-
-// console.log(options);
-
-let counter = 0
-
-for (let key in options) {
-    if (typeof(options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-        counter++;
-    }
+function compareNum(a, b) { // сравненеие чисел
+    return a - b;
 }
 
-console.log(counter)
+
+// arr[99] =0;
+// console.log(arr.length);
+// console.log(arr)
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop(); //Удаляет последний элемент из массива
+// arr.push(10); // Добавляет последний элемент в массив
+
+// console.log(arr);
+
+// for (let i =0; i < arr.length; i++) { //цикл все элементы массива пока не закончится длина массива.
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) { // for of .. цикл  
+//     console.log(value);
+// }
+
+const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; '));
