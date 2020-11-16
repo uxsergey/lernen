f24b59c6b15030451d3e8675dc3e384afff53a7a
Sergey <example@gmail.com>
2020-11-16 15:03:44 +0100

    Lesson 23: OOP Basics

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,94 +1,30 @@
 "use strict";
 
-// let a = 5,
-//     b = a;
+// let str = 'some';
+// let strObj = new String(str);
 
-// b = b + 5;
+// console.log(typeof(str));
+// console.log(typeof(strObj));
 
-// console.log(b);
-// console.log(a);
+// console.dir([1, 2, 3])
 
-// const obj = {
-//     a: 5,
-//     b: 1
+const solder = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello!');
+    }
+};
+const john = Object.create(solder);
+Object.setPrototypeOf(john, solder);
+john.sayHello();
+
+// const john = {
+//     health: 100
 // };
 
-// const copy = obj; // Передает ссылку
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) { //функция поверхостная копия объекта
-    let objCopy = {};
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const num = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 8
-    }
-};
-
-const newNum = copy(num);
-newNum.a = 10;
-newNum.c.x = 10;
-
-// console.log(newNum);
-// console.log(num);
+// // john.__proto__ = solder;//устаревшее
 
 
-// object assign method
-const add = {
-    d: 17,
-    e: 23
-};
 
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-//  console.log(add);
-//  console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'dsgsdfgsd';
-console.log(newArray);
-console.log(oldArray);
-
-//// Spred Operathors ES6
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const nummber = [2, 5, 7];
-log(...nummber);
-
-const arr2 = ['a', 'b'];
-const newArr = [...arr2];
-console.log(newArr);
-
-const q = {
-    one: 1,
-    two: 2
-};
-const newObj = {...q };
-console.log(newObj);
+// console.log(john.armor);
