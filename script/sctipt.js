009c18b357dcadaab0c33e4f97ed66304eed34d0
Sergey <example@gmail.com>
2020-11-16 12:13:38 +0100

    Lesson 23:  pass by value and pass by reference

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,36 +1,94 @@
 "use strict";
 
-const arr = [2, 63, 6, 18];
-arr.sort(compareNum);
-console.log(arr);
+// let a = 5,
+//     b = a;
 
-function compareNum(a, b) { // сравненеие чисел
-    return a - b;
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // Передает ссылку
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) { //функция поверхостная копия объекта
+    let objCopy = {};
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
 }
 
+const num = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 8
+    }
+};
 
-// arr[99] =0;
-// console.log(arr.length);
-// console.log(arr)
+const newNum = copy(num);
+newNum.a = 10;
+newNum.c.x = 10;
 
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// });
+// console.log(newNum);
+// console.log(num);
 
-// arr.pop(); //Удаляет последний элемент из массива
-// arr.push(10); // Добавляет последний элемент в массив
 
-// console.log(arr);
+// object assign method
+const add = {
+    d: 17,
+    e: 23
+};
 
-// for (let i =0; i < arr.length; i++) { //цикл все элементы массива пока не закончится длина массива.
-//     console.log(arr[i]);
-// }
+const clone = Object.assign({}, add);
 
-// for (let value of arr) { // for of .. цикл  
-//     console.log(value);
-// }
+clone.d = 20;
 
-const str = prompt("", "");
-const products = str.split(", ");
-products.sort();
-console.log(products.join('; '));
+//  console.log(add);
+//  console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'dsgsdfgsd';
+console.log(newArray);
+console.log(oldArray);
+
+//// Spred Operathors ES6
+
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const nummber = [2, 5, 7];
+log(...nummber);
+
+const arr2 = ['a', 'b'];
+const newArr = [...arr2];
+console.log(newArr);
+
+const q = {
+    one: 1,
+    two: 2
+};
+const newObj = {...q };
+console.log(newObj);
