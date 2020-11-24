9a300d5c520a180ed435a5eb854f079e1ce08ba9
Sergey <segeyxdesign@gmail.com>
2020-11-24 13:46:03 +0100

    Lesson 37: ClassList

--- lessons/script/sctipt.js
+++ lessons/script/sctipt.js
@@ -1,12 +1,57 @@
 'use strict';
-const p = document.querySelectorAll('p');
-console.log(p);
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
 
-function loadScript(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
-loadScript('script/test.js');
-loadScript('script/some.js');
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+// console.log(btns[0].classList.add('red', 'green'));
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+// console.log(btns[0].className);
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red')
+// }
+
+// btns[0].addEventListener('click', () => { /// Добавляем событие на клик первой кнопки при кототром
+//     if (!btns[1].classList.contains('red')) { ///мы следим что у второй кнопки нет класса 'red'
+//         btns[1].classList.add('red'); ///При необходимости добавляем его
+//     } else {
+//         btns[1].classList.remove('red'); // Если он уже есть удаляем
+//     }
+// });
+
+
+btns[0].addEventListener('click', () => {
+    btns[1].classList.toggle('red');
+});
+
+// wrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.tagName == 'BUTTON') { //Google recomendation. Пример делегирования события
+//         console.log('hello')
+//     }
+// });
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches('button.red')) { //Google recomendation. Пример делегирования события
+        console.log('hello');
+    }
+});
+
+// btns.forEach(btn => { // Пример ошибки делегирования
+//     btn.addEventListener('click', () => {
+//         console.log('hello')
+//     })
+// })
+
+const btn = document.createElement('button');
+btn.classList.add('green');
+wrapper.append(btn); // метод append позволяет поместить btn в wrapper
+
+
+// wrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.classList.contains('blue')) {
+//         console.log('hello');
+//     }
+// });
