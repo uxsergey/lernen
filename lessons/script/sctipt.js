d86cfe56a46369bdfa77ad6e1dc77a8f93a61d96
Sergey <segeyxdesign@gmail.com>
2020-11-25 13:29:30 +0100

    Lesson 40: Date

--- lessons/script/sctipt.js
+++ lessons/script/sctipt.js
@@ -1,46 +1,28 @@
 'use strict';
-// const timeId = setTimeout(logger, 2222),
-const btn = document.querySelector('.btn');
-let timerId,
-    i = 0;
+const now = new Date('2020-11-25');
+// new Date.parse('2020-11-25');
+
+//Методы сеттеры
+console.log(now.setHours(140)); //Установить время
+console.log(now);
+
+//Методы геттеры
+
+// console.log(now);
+// console.log(now.getFullYear()); //Получение полного года
+// console.log(now.getMounth()); //Получение полного месяца
+// console.log(now.getHours()); //Получение времени
+// console.log(now.getDate()); //Получение дня
+// console.log(now.getDay()); //Получение номера дня недел.неделя начинается с воскр.
+// console.log(now.getUTCHours()); //Получениевремени по другому часовому поясу
+// console.log(now.getTimezoneOffset()); //Получение рахницы между часовым поясом и UTC
+// console.log(now.getTime()); //Получение количества милисекунд с 1970-01-01
 
 
-// btn.addEventListener('click', () => {
-//     // const timerId = setTimeout(logger, 2000);    
-//     timerId = setInterval(logger, 500);
-// })
-
-
-
-// function logger() {
-//     if (i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('Hello');
-//     i++;
-
-// }
-
-// let id = setTimeout(function log() {
-//     console.log('Hello');
-//     id = setTimeout(log, 500);
-// }, 500);
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
-    const id = setInterval(frame, 4);
-
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-
-        }
-    }
-
+let start = new Date();
+for (let i = 0; i < 10000; i++) {
+    let some = i ** 3;
 }
-btn.addEventListener('click', myAnimation);
+let end = new Date();
+
+alert(`Цикл отработал за ${end-start} милисекунд`)
