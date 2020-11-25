84bf2cda2a0bbac93c1c2a8218b94dc15d3d8f32
Sergey <segeyxdesign@gmail.com>
2020-11-25 12:23:40 +0100

    Lesson 39: setTimeout and setInterval

--- lessons/script/sctipt.js
+++ lessons/script/sctipt.js
@@ -1,57 +1,46 @@
 'use strict';
-const btns = document.querySelectorAll('button'),
-    wrapper = document.querySelector('.btn-block');
-
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(0));
-// console.log(btns[0].classList.add('red', 'green'));
-// console.log(btns[1].classList.add('red'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
-// console.log(btns[0].className);
-
-// if (btns[1].classList.contains('red')) {
-//     console.log('red')
-// }
-
-// btns[0].addEventListener('click', () => { /// Добавляем событие на клик первой кнопки при кототром
-//     if (!btns[1].classList.contains('red')) { ///мы следим что у второй кнопки нет класса 'red'
-//         btns[1].classList.add('red'); ///При необходимости добавляем его
-//     } else {
-//         btns[1].classList.remove('red'); // Если он уже есть удаляем
-//     }
-// });
+// const timeId = setTimeout(logger, 2222),
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
 
 
-btns[0].addEventListener('click', () => {
-    btns[1].classList.toggle('red');
-});
-
-// wrapper.addEventListener('click', (event) => {
-//     if (event.target && event.target.tagName == 'BUTTON') { //Google recomendation. Пример делегирования события
-//         console.log('hello')
-//     }
-// });
-
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.matches('button.red')) { //Google recomendation. Пример делегирования события
-        console.log('hello');
-    }
-});
-
-// btns.forEach(btn => { // Пример ошибки делегирования
-//     btn.addEventListener('click', () => {
-//         console.log('hello')
-//     })
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000);    
+//     timerId = setInterval(logger, 500);
 // })
 
-const btn = document.createElement('button');
-btn.classList.add('green');
-wrapper.append(btn); // метод append позволяет поместить btn в wrapper
 
 
-// wrapper.addEventListener('click', (event) => {
-//     if (event.target && event.target.classList.contains('blue')) {
-//         console.log('hello');
+// function logger() {
+//     if (i === 3) {
+//         clearInterval(timerId);
 //     }
-// });
+//     console.log('Hello');
+//     i++;
+
+// }
+
+// let id = setTimeout(function log() {
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+    const id = setInterval(frame, 4);
+
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+
+        }
+    }
+
+}
+btn.addEventListener('click', myAnimation);
