5a67c552c9687fba96632c1f825481586930f68a
Sergey <segeyxdesign@gmail.com>
2020-12-01 17:35:12 +0100

    Lesson 48: Add cards with Classes

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -186,8 +186,7 @@ window.addEventListener('DOMContentLoaded', () => {
   function openModal() {
     modal.classList.add('show');
     modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
-    clearInterval(modalTimerId);
+    document.body.style.overflow = 'hidden'; // clearInterval(modalTimerId);
   }
 
   modalTrigger.forEach(btn => {
@@ -216,8 +215,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (e.code === 'Escape' && modal.classList.contains('show')) {
       closeModal();
     }
-  });
-  const modalTimerId = setTimeout(openModal, 8000);
+  }); // const modalTimerId = setTimeout(openModal, 8000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -226,7 +224,45 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  window.addEventListener('scroll', showModalByScroll); //Clsses for card
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.img = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = ` <div class="menu__item">
+            <img src="${this.src}" alt="${this.alt}">
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>
+    `;
+      this.parent.append(element);
+    }
+
+  }
+
+  new MenuCard("img/tabs/vegy.jpg", "Freegan", 'Меню "Фриган"', "В качестве источника продуктов  фриганы используют свалки, мусорные контейнеры и т. п.", 9, ".menu .container").render();
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 12, ".menu .container").render();
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 14, ".menu .container").render();
 });
 
 /***/ })
--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -97,7 +97,7 @@ window.addEventListener('DOMContentLoaded', () => {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId);
+        // clearInterval(modalTimerId);
     }
 
     modalTrigger.forEach(btn => {
@@ -131,7 +131,7 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
-    const modalTimerId = setTimeout(openModal, 8000);
+    // const modalTimerId = setTimeout(openModal, 8000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -139,5 +139,68 @@ window.addEventListener('DOMContentLoaded', () => {
             window.removeEventListener('scroll', showModalByScroll);
         }
     }
+
     window.addEventListener('scroll', showModalByScroll);
+
+    //Clsses for card
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.img = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+
+        }
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML =
+                ` <div class="menu__item">
+            <img src="${this.src}" alt="${this.alt}">
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+        </div>
+    `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "Freegan",
+        'Меню "Фриган"',
+        "В качестве источника продуктов  фриганы используют свалки, мусорные контейнеры и т. п.",
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        12,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        14,
+        ".menu .container"
+    ).render();
+
 });
