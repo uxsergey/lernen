60fd6170794b798678464fcbc6b7f5717c3ef651
Sergey <segeyxdesign@gmail.com>
2020-12-01 14:44:58 +0100

    Lesson 46:  This function

--- lessons/script/sctipt.js
+++ lessons/script/sctipt.js
@@ -1,25 +1,85 @@
 'use strict';
-// const num = new Number(3);
-// console.log(num);
+//1)Обычная функция this = window, но если use strict -undefined
+//2) Контекст у методов обьекта = сам обьект
+//3) this в конструкторах и классах- это новый экзэпляр объекта (!!!)
+//4) Ручная привязка this: call, apply, bindconst
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}, ${this.id}`)
-    };
-}
 
-User.prototype.exit = function() {
-    console.log(`This ${this.name} is Leave System`)
+// function showThis(a, b) {
+//     console.log(this);
+
+//     function sum() {
+//         console.log(this);
+//         return a + b
+//     }
+//     console.log(sum());
+// }
+// showThis(4, 5);
+
+// const obj = {
+
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         function showed() {
+//             console.log(this);
+//         }
+//         showed();
+//     }
+// };
+// obj.sum();
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('hello!' + this.name);
+
+//     };
+// }
+// let ivan = new User('Ivan', 23);
+
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname)
+// }
+// const user = {
+
+//     name: 'John'
+// };
+// sayName.call(user, 'smith');
+// sayName.apply(user), ['smith'];
+
+// function count(num) {
+//     return this * num;
+
+// }
+// const double = count.bind(2);
+// console.log(double(3));
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    this.style.backgroundColor = 'red'
+});
+const obj = {
+    num: 5,
+    sayNumber: function() {
+
+        ///У Стрелочной функции нет своего контекста вызова.Она берет его от своего родителя
+
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
 };
 
-const ivan = new User('Ivan', 30);
-const alex = new User('Alex', 24);
+obj.sayNumber();
 
-ivan.hello();
-alex.hello();
-
-console.log(ivan);
-console.log(alex);
+const double = a => a * 2;
+console.log(double(3));
