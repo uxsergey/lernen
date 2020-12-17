d755290b5acc6b60c3fce76d57912728bffbc49c
Sergey <segeyxdesign@gmail.com>
2020-12-17 21:58:15 +0100

    Lesson 64: Dots Navigation

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -2345,6 +2345,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const sliderCounter = document.querySelector('.offer__slider-counter'),
         slide = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = sliderCounter.querySelector('.offer__slider-prev'),
         next = sliderCounter.querySelector('.offer__slider-next'),
         total = sliderCounter.querySelector('#total'),
@@ -2371,6 +2372,50 @@ window.addEventListener('DOMContentLoaded', () => {
   slide.forEach(slide => {
     slide.style.width = width;
   });
+  slider.style.position = 'relative';
+  const indicators = document.createElement('ol'),
+        dots = [];
+  indicators.classList.add('corusel-indicators');
+  indicators.style.cssText = `    position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+  slider.append(indicators);
+
+  for (let i = 0; i < slide.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+        `;
+
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
   next.addEventListener('click', () => {
     if (offset == +width.slice(0, width.length - 2) * (slide.length - 1)) {
       //500px
@@ -2392,6 +2437,9 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
   });
   prev.addEventListener('click', () => {
     if (offset == 0) {
@@ -2413,6 +2461,26 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
+  });
+  dots.forEach(dot => {
+    dot.addEventListener('click', e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slide.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = slideIndex;
+      }
+
+      dots.forEach(dot => dot.style.opacity = '.5');
+      dots[slideIndex - 1].style.opacity = 1;
+    });
   }); ///Simple version
   // showSlides(slideIndex);
   // if (slide.length < 10) {
